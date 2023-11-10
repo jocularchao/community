29d29e2702e71bdd116410357b51d0e03441b9d0
jocularchao <jocularchao@gmail.com>
2023-11-10 22:40:12 +0800

    first create

---
+++ community_backend/src/test/java/top/jocularchao/community_backend/CommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jocularchao.community_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
